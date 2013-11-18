df4341214d7f78191209fd29dceafb707f733a5f
Lucas Caton <lucascaton@gmail.com>
2013-11-18 17:03:02 +1000

    Initial Rails directory structure

---
+++ Gemfile
@@ -0,0 +1,36 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.1'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'turbolinks'
+gem 'haml-rails'
+gem 'pg'
+gem 'less-rails'
+gem 'twitter-bootstrap-rails'
+
+group :development do
+  gem 'annotate'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'thin'
+  gem 'meta_request'
+end
+
+group :development, :test do
+  gem 'pry-meta'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'forgery'
+  gem 'poltergeist'
+  gem 'rspec-rails'
+  gem 'simplecov', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyWiki::Application.config.secret_key_base = '7391462c8fa764e99bb1bdfa51809b688eff0160498fe641a69b11075549abff66e308d7a4eba145f173051d8e8afe29b80ab1db510090e5584b8190a6f31c32'
