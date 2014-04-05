aadaea7570b173fb61a653b128095e8afcd90cc7
Lucas Caton <lucascaton@gmail.com>
2014-04-05 16:29:10 +1000

    Fixing new required config in Paperclip

--- app/models/wiki_detail.rb
+++ app/models/wiki_detail.rb
@@ -1,5 +1,6 @@
 class WikiDetail < ActiveRecord::Base
-  has_attached_file :logo, styles: { small: '250>' }
+  has_attached_file    :logo, styles: { small: '250>' }
+  validates_attachment :logo, content_type: { content_type: /\Aimage\/.*\Z/ }
 
   belongs_to :initial_page, class_name: 'Page'
 end
